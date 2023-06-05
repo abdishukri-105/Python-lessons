a7aaafd600f7d16ba59544977dde186e1f7f53f4
abdishukri <abdishukrimohamed105@gmail.com>
2023-06-05 21:32:06 +0300

    finished list compression

--- Tutorial/List.py
+++ Tutorial/List.py
@@ -2,3 +2,17 @@
 
 mylist = ["apple", "banana", "cherry"]
 
+#  List Comprehension
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+# print(newlist)
+
+# with list compression
+
+newlist = [x for x in fruits if "a" in x]    
+#  this is like map in js  -  produce new array that meets the condition
