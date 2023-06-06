d4b736f72dc4c19a2ffcc6c8babab1c620acc3ef
abdishukri <abdishukrimohamed105@gmail.com>
2023-06-06 20:16:18 +0300

    finished func

---
+++ Tutorial/functions.py
@@ -0,0 +1,44 @@
+
+# functions
+
+def my_function():
+    print("Hello, from my function")
+
+my_function()
+
+# arguments
+
+def my_function(fname):
+  print(fname + " mohamed")
+
+
+my_function("shukri")
+
+# Keyword Arguments
+
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+# Default Parameter Value
+
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
+
+# return values
+
+def my_function(x):
+  return 5 * x
+
+
+
+
+
+
