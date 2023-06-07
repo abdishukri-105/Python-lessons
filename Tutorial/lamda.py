2b4671ed1a6e2559a6b0d6e1210a2f71823e556d
abdishukri <abdishukrimohamed105@gmail.com>
2023-06-07 21:45:57 +0300

    done lamda

---
+++ Tutorial/lamda.py
@@ -0,0 +1,17 @@
+#  lambda function 
+
+# A lambda function is a small anonymous function.
+
+# syntax
+
+lambda arguments : expression
+
+x = lambda a : a + 10
+print(x(5))
+
+# labda args
+
+x = lambda a, b : a * b 
+
+
+
